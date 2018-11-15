1132bf8e773f83bb3d8c292d26049cb4976ecc03
Zach Rash <zacrash@s-169-232-238-165.resnet.ucla.edu>
2018-11-15 01:57:41 -0800

    Update after testing

--- src/app.py
+++ src/app.py
@@ -1,82 +1,105 @@
 import logging
+import json
 
-from flask import Flask, request
+from flask import Flask, request, Response, jsonify
 
 from db import Db
+from location import IndoorLocation, OutdoorLocation
 
 app = Flask(__name__)
 db = Db()
 
-@app.route('/addUser', methods=['POST'])
+
+@app.route('/addUser', methods=['GET'])
 def add_user():
     """Add a new user to db"""
     user_name = request.args.get('user_name')
     if not user_name:
         logging.info('/addUser: no user name')
-        return 'Must provide user name'
-    db.add_user(user_name)
-    return "Signed up!"
+        return Response('Must provide user name', status=400)
+    added = db.add_user(user_name)
+    if added:
+        return Response("Signed up!", status=200)
+    return Response('User already exists', status=400)
 
-@app.route('/addFriend', methods=['POST'])
+
+@app.route('/addFriend', methods=['GET'])
 def add_friend():
     """Add user to friends list"""
     user_name = request.args.get('user_name')
     friend_name = request.args.get('friend_name')
     if not user_name or not friend_name:
         logging.info('/add_friend: no user name or friend name')
-        return 'Must provide user name and friend name'
-    db.add_friend(user_name, friend_name)
-    return 'Added!'
+        return Response('Must provide user name and friend name', status=400)
+    res = db.add_friend(user_name, friend_name)
+    if res:
+        return Response('Added!', status=200)
+    return Response("Cannot add friend to user's friends list, user does not exist", status=400)
 
 
-@app.route('/deleteFriend', methods=['POST'])
+@app.route('/deleteFriend', methods=['GET'])
 def delete_friend():
     """Remove user from friends list"""
     user_name = request.args.get('user_name')
     friend_name = request.args.get('friend_name')
     if not user_name or not friend_name:
         logging.info('/delete_friend: no user name or friend name')
-        return 'Must provide user name and friend name'
-    db.delete_friend(user_name, friend_name)
-    return 'Removed!'
+        return Response('Must provide user name and friend name', status=400)
+    res = db.delete_friend(user_name, friend_name)
+    if res:
+        return Response('Removed!', status=200)
+    return Response('Friend does not exist', status=400)
 
 
 @app.route('/registerLocation', methods=['POST'])
 def register():
     """Register a user's most recent location"""
     user_name = request.args.get('user_name')
-    location = request.args.get('location')
-    db.set_location(user_name, location)
-    return "Updated!"
+    location = request.json
+    res = db.set_location(user_name, location)
+    if res:
+        return Response('Updated!', status=200)
+    return Response('No such user', status=400)
 
-@app.route('/lookup')
+
+@app.route('/lookup', methods=['GET'])
 def lookup_loc():
     """Look up a user's current location"""
     user_name = request.args.get('user_name')
     friend_name = request.args.get('friend_name')
     if not friend_name:
+        print('no friend_name')
         logging.info('/lookup_loc: no friend name')
-        return 'Must provide a friend name'
+        return Response('Must provide a friend name', status=400)
 
-    # Ensure user is allowed to view location still
+    # Ensure user exists is allowed to view location still
     friends_list = db.get_friends_list(user_name)
+    if friends_list is None:
+        return Response('No such user', status=400)
+
+    if not db.location_available(user_name):
+        return Response('Friend has location toggled off', status=401)
+
     if friend_name not in friends_list:
+        print('access denied')
         logging.info('/lookup_loc: illegal friend lookup')
-        return "Not authroized to view this user's location"
+        return Response("Not authroized to view this user's location", status=401)
 
     location = db.get_location(friend_name)
-    return str(location)
+    if location is None:
+        return Response('No such user', status=400)
+    return Response(json.dumps(location), status=200)
 
 
-@app.route('/toggle', methods=['POST'])
+@app.route('/toggle', methods=['GET'])
 def toggle_loc():
     """Toggle user location on and off"""
     user_name = request.args.get('user_name')
     if not user_name:
         logging.info('/toggle: No user name')
-        return 'Must provide a user name'
+        return Response('Must provide a user name', status=400)
     db.toggle(user_name)
-    return "Toggled!"
+    return Response("Toggled!", status=200)
 
 
 if __name__ == '__main__':
--- src/db.py
+++ src/db.py
@@ -8,51 +8,88 @@ os.environ['MONGO_URI'] = 'mongodb://localhost:27017'
 os.environ['USER_TABLE'] = 'User'
 
 
-class Db:
+class Db(object):
     MONGO_URI = os.environ['MONGO_URI']
     USER_TABLE = os.environ['USER_TABLE']
+    _db = None
+
+    def __new__(cls):
+        if cls._db == None:
+            cls._db = object.__new__(cls)
+        return cls._db
 
     def __init__(self):
-        self._db = MongoClient(self.MONGO_URI)
+        self._db = MongoClient(self.MONGO_URI)['wya']
 
     def add_user(self, user_name):
         """Add a new user"""
-        if self._db[self.USER_TABLE].find({'user': user_name}):
-            return
-            
+        if self._db[self.USER_TABLE].find_one({'user': user_name}) is not None:
+            return False
+
         self._db[self.USER_TABLE].insert_one({'user': user_name})
+        return True
 
     def get_friends_list(self, user_name):
         """Get list of friends for a given user"""
-        return self._db[self.USER_TABLE].find({'user': user_name}).get('friendsList')
+        l = self._db[self.USER_TABLE].find_one({'user': user_name})
+        if l is not None:
+            return l.get('friendsList')
+        return None
 
     def add_friend(self, user_name, friend_name):
         """Add friend to user's friends list"""
-        self._db[self.USER_TABLE].update_one({'user': user_name}, {'$push': {'friendsList': friend_name}})
+        res = self._db[self.USER_TABLE].update_one({'user': user_name}, {'$push': {'friendsList': friend_name}})
+        # Check if user exists
+        if res.matched_count == 0:
+            return False
+        return True
 
     def delete_friend(self, user_name, friend_name):
         """Delete a friend from a user's friends list"""
         # Unfortunately we need to query the friends_list, search for the user, and remove
         friends_list = self.get_friends_list(user_name)
+        print(friends_list)
         # If no friends, we are done
         if not friends_list:
-            return
+            return False
         try:
-            friends_list.remove('friend_name')
+            friends_list.remove(friend_name)
         except ValueError:
             logging.info('Could not remove {} because user {} does not have them in friends list'.format(friend_name, user_name))
-            return
+            print('here')
+            return False
         
         self._db[self.USER_TABLE].update_one({'user': user_name}, {'$set': {'friendsList': friends_list}})
+        return True
     
     def get_location(self, friend_name):
         """Query database for location of specified user"""
-        return self._db[self.USER_TABLE].find({'user': friend_name}).get('location')
+        l = self._db[self.USER_TABLE].find_one({'user': friend_name})
+        if l is not None:
+            return l.get('location')
+        return None
 
     def set_location(self, user_name, location):
         """Update user with new location from device"""
-        self._db[self.USER_TABLE].update_one({'user': user_name}, {'$set': {'location': location}})
+        res = self._db[self.USER_TABLE].update_one({'user': user_name}, {'$set': {'location': location}})
+        if res.matched_count == 0:
+            return False
+        return True
 
     def toggle(self, user_name):
         """Toggle user's location sharing"""
-        self._db[self.USER_TABLE].update_one({'user': user_name}, {'$set': {'location_sharing': False}})
+        l = self._db[self.USER_TABLE].find_one({'user': user_name})
+        if l is None:
+            return None
+        toggle = l.get('location_sharing')
+        if toggle is not None:
+            self._db[self.USER_TABLE].update_one({'user': user_name}, {'$set': {'location_sharing': not toggle}})
+        else:
+            self._db[self.USER_TABLE].update_one({'user': user_name}, {'$set': {'location_sharing': False}})
+
+    def location_available(self, user_name):
+        """Check if user's location is toggled off"""
+        l = self._db[self.USER_TABLE].find_one({'user': user_name})
+        if l is not None:
+            return l.get('location_sharing')
+        return None
