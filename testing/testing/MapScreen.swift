990a1f59ceee362558d9b31c3b9ffc550ad522de
Arti Patankar <rtpatankar24@gmail.com>
2018-11-15 17:33:54 -0800

    added preliminary location and route functionalities with MapKit

---
+++ testing/testing/MapScreen.swift
@@ -0,0 +1,93 @@
+//
+//  MapScreen.swift
+//  testing
+//
+//  Created by Arti Patankar on 11/14/18.
+//  Copyright © 2018 com.example. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapScreen: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let locationManager = CLLocationManager()
+    let range:Double = 1000
+    let friends = ["An", "Ariel", "Brad", "Zach"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkLocationServices()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "customcell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customcell", for: indexPath as IndexPath)
+        cell.textLabel?.text = friends[indexPath.item]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "mapToNav", sender: self)
+    }
+    
+    func setUpLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func centerView() {
+        if let location = locationManager.location?.coordinate {
+            // center is user's location, set bounds
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: range, longitudinalMeters: range)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setUpLocationManager()
+            checkLocationAuthorization()
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerView()
+            locationManager.startUpdatingLocation()
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
+        }
+    }
+}
+
+extension MapScreen: CLLocationManagerDelegate {
+    // called whenever user's location updates
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion.init(center: center, latitudinalMeters: range, longitudinalMeters: range)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    // called whenever permission changes
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+}
+
---
+++ testing/testing/NavigationScreen.swift
@@ -0,0 +1,82 @@
+//
+//  NavigationScreen.swift
+//  testing
+//
+//  Created by Arti Patankar on 11/15/18.
+//  Copyright © 2018 com.example. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class NavigationScreen: UIViewController {
+    
+    @IBOutlet weak var navigationView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationView.delegate = self
+        navigationView.showsPointsOfInterest = true
+        navigationView.showsUserLocation = true
+        setUpLocationManager()
+        generateDirections()
+    }
+    
+    // without this, the overlay is not visible
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.gray
+        renderer.lineWidth = 5
+        
+        return renderer
+    }
+    
+    func setUpLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func generateDirections() {
+        locationManager.startUpdatingLocation()
+        
+        let location = locationManager.location?.coordinate
+        let destination = CLLocationCoordinate2DMake(34.0688, -118.4440)
+    
+        let start = MKPlacemark(coordinate: location!)
+        let end = MKPlacemark(coordinate: destination)
+    
+        let startItem = MKMapItem(placemark: start)
+        let endItem = MKMapItem(placemark: end)
+        
+        let endPin = MKPointAnnotation()
+        endPin.coordinate = destination
+        endPin.title = "X"
+        navigationView.addAnnotation(endPin)
+        
+        let request = MKDirections.Request()
+        request.source = startItem
+        request.destination = endItem
+        request.transportType = .walking
+        
+        let directions = MKDirections(request: request)
+        directions.calculate(completionHandler: {
+            response, error in
+            guard let response = response else {
+                return
+            }
+        
+            let route = response.routes[0]
+            self.navigationView.addOverlay(route.polyline, level: .aboveRoads)
+            
+            let region = route.polyline.boundingMapRect
+            self.navigationView.setRegion(MKCoordinateRegion(region), animated: true)
+        })
+    }
+}
+
+extension NavigationScreen: MKMapViewDelegate {}
+
+extension NavigationScreen: CLLocationManagerDelegate {}
