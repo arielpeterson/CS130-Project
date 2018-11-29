b7622c05c6d24ee393e82e176caf932d8795dab9
antnguyen <thaiannguyen91@gmail.com>
2018-11-29 02:43:15 -0800

    Update image.py
    - added lines mask
    - added building and floor in image file name

--- src/image.py
+++ src/image.py
@@ -14,7 +14,7 @@ class CvExtractor:
     def __init__(self, image_size=(960,960)):
         self.image_size = image_size
     
-    def extract_image(self, image_file):
+    def extract_image(self, image_file, building, floor):
         """
         API entrypoint for extracting model from image.
         Takes an image file and returns a processed image
@@ -70,10 +70,47 @@ class CvExtractor:
         # Crop
         image = image[b:t, l:r]
 
-
+        # Create 2D image white mask to filter lines
+        height, width, _ = image.shape
+        mask = np.ones([height, width], dtype=np.uint8) * 255
+        full_lines = []
+        
+        for line in lines:
+            for x1,y1,x2,y2 in line:
+                full_lines.append((x1, y1))
+                full_lines.append((x2, y2))
+                
+                # vertical lines
+                if x1 - x2 == 0:
+                    for y in range(y1 + 1, y2):
+                        full_lines.append((x1, y))
+                    continue
+                    
+                # other lines
+                m = (y1 - y2) / (x1 - x2)
+            
+                for x in range(x1 + 1, x2):
+                    # horizontal lines
+                    if int(m) == 0:
+                        y = y1
+                        
+                    # inclined lines
+                    else:
+                        y = np.floor(m * (x - x1) + y1)
+                        if y >= height:
+                            y = height - 1
+                    full_lines.append((x, y))
+                
+        for pixel in full_lines:
+            x = int(pixel[0])
+            y = int(pixel[1])
+            mask[y][x] = 0
+        
+        # Apply mask
+        image = mask
 
         # Save
-        fn = str(time.time()) + '.png'
+        fn = str(building) + '_floor' + str(floor) + '_' + str(time.time()) + '.png'
         image_dir = os.environ.get('IMAGE_DIR')
         cv2.imwrite(os.path.join(image_dir, fn), image)
         return fn
@@ -81,4 +118,4 @@ class CvExtractor:
 
 if __name__ == '__main__':
     model = CvExtractor()
-    model.extract_image('test.jpg')
+    model.extract_image('test.jpg', 'MooreHall', 1)
