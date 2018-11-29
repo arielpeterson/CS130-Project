ea4ce60c9b7bb89272f47e21f0a3dfef70efe6e9
Ariel Peterson <ariel.peterson1995@gmail.com>
2018-11-28 17:22:03 -0800

    Changed name of file to QueryService.swfit a
    Added functions for client to access server. addUser and addFriend work correctly

---
+++ src/Xcode/QueryService.swift
@@ -0,0 +1,186 @@
+//
+//  QueryService.swift
+//  HTTP_POST_EXAMPLE
+//
+//  Created by Ariel Peterson on 11/15/18.
+//  Copyright © 2018 Ariel Peterson and An Mary Nguyen. All rights reserved.
+
+import UIKit
+import Foundation
+
+// Must change each time we run ngrok
+let SERVER = "http://c02c0a92.ngrok.io"
+
+class QueryService {
+    typealias JSONDictionary = [String: Any]
+    var errorMessage = ""
+    
+    let defaultSession = URLSession(configuration: .default)
+    var dataTask: URLSessionDataTask?
+    
+    // Adds a new user to the database.
+    func addUser(user_name :String){
+        dataTask?.cancel()
+        let url_string = SERVER+"/addUser?user_name="+user_name
+        let url = NSURL(string: url_string)!
+        
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "GET"
+        
+        dataTask = defaultSession.dataTask(with: request as URLRequest) {
+            data, response, error in
+            defer { self.dataTask = nil }
+            if let error = error {
+                self.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
+                print(self.errorMessage)
+            } else if let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200
+            {
+                print(data)
+                print(response)
+                
+            }
+        }
+        dataTask?.resume()
+    }
+    
+    // Adds friend to user's friends list.
+    func addFriend(user_name :String, friend_name :String){
+        dataTask?.cancel()
+        let url_string = SERVER+"/addFriend?user_name="+user_name+"&friend_name="+friend_name
+        let url = NSURL(string: url_string)!
+        
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "GET"
+        
+        dataTask = defaultSession.dataTask(with: request as URLRequest) {
+            data, response, error in
+            defer { self.dataTask = nil }
+            if let error = error {
+                self.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
+                print(self.errorMessage)
+            } else if let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200
+            {
+                print(data)
+                print(response)
+                
+            }
+        }
+        dataTask?.resume()
+    }
+    
+    // Remove friend from user's friend list
+    func deleteFriend(user_name :String, friend_name :String){
+        dataTask?.cancel()
+        let url_string = SERVER+"/deleteFriend?user_name="+user_name+"&friend_name="+friend_name
+        let url = NSURL(string: url_string)!
+        
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "GET"
+        
+        dataTask = defaultSession.dataTask(with: request as URLRequest) {
+            data, response, error in
+            defer { self.dataTask = nil }
+            if let error = error {
+                self.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
+                print(self.errorMessage)
+            } else if let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200
+            {
+                print(data)
+                print(response)
+                
+            }
+        }
+        dataTask?.resume()
+    }
+    
+    // Register a user's most recent location.
+    func registerLocation(user_name :String, location :JSONDictionary){
+        dataTask?.cancel()
+        let url_string = SERVER+"/registerLocation"
+        let url = NSURL(string: url_string)!
+        
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "POST"
+        // request.httpBody =
+        // Add body to include user_name and location
+        
+        dataTask = defaultSession.dataTask(with: request as URLRequest) {
+            data, response, error in
+            defer { self.dataTask = nil }
+            if let error = error {
+                self.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
+                print(self.errorMessage)
+            } else if let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200
+            {
+                print(data)
+                print(response)
+                
+            }
+        }
+        dataTask?.resume()
+    }
+    
+    // Looks up location of a friend for a given user.
+    func lookup(user_name :String, friend_name :String){
+        dataTask?.cancel()
+        let url_string = SERVER+"/lookup?user_name="+user_name+"&friend_name="+friend_name
+        let url = NSURL(string: url_string)!
+        
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "GET"
+        
+        dataTask = defaultSession.dataTask(with: request as URLRequest) {
+            data, response, error in
+            defer { self.dataTask = nil }
+            if let error = error {
+                self.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
+                print(self.errorMessage)
+            } else if let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200
+            {
+                print(data)
+                print(response)
+                
+            }
+        }
+        dataTask?.resume()
+    }
+    
+    // Toggle user's location sharing on and off.
+    func toggle(user_name :String){
+        dataTask?.cancel()
+        let url_string = SERVER+"/toggle?user_name="+user_name
+        let url = NSURL(string: url_string)!
+        
+        let request = NSMutableURLRequest(url: url as URL)
+        request.httpMethod = "GET"
+        
+        dataTask = defaultSession.dataTask(with: request as URLRequest) {
+            data, response, error in
+            defer { self.dataTask = nil }
+            if let error = error {
+                self.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
+                print(self.errorMessage)
+            } else if let data = data,
+                let response = response as? HTTPURLResponse,
+                response.statusCode == 200
+            {
+                print(data)
+                print(response)
+                
+            }
+        }
+        dataTask?.resume()
+    }
+    
+    
+}
--- src/Xcode/wya/QueryService.swift
+++ src/Xcode/wya/QueryService.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HttpRequest.swift
 //  HTTP_POST_EXAMPLE
 //
 //  Created by Ariel Peterson on 11/15/18.
@@ -10,27 +10,27 @@ import UIKit
 import Foundation
 
 // Must change each time we run ngrok
-let SERVER = "http://66e5dd3c.ngrok.io"
+let SERVER = "http://ff82a439.ngrok.io"
 
-class ViewController: UIViewController {
+class QueryService {
     typealias JSONDictionary = [String: Any]
     var errorMessage = ""
     
     let defaultSession = URLSession(configuration: .default)
     var dataTask: URLSessionDataTask?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func addUser(user_name: String) {
+        
         // Do any additional setup after loading the view, typically from a nib.
-        let user_name = "Ariel"
+        //let user_name = "Ariel"
         
         dataTask?.cancel()
-        let url = NSURL(string: SERVER+"/addUser?user_name=Ariel")!
-        //let message = "user_name=" + user_name
-
+        let url = NSURL(string: SERVER+"/addUser?user_name="+user_name)! // change to use user_name from function parameter
+        
+        
         let request = NSMutableURLRequest(url: url as URL)
         request.httpMethod = "GET"
-        //request.httpBody = message.data(using: String.Encoding.utf8)
+        
         
         dataTask = defaultSession.dataTask(with: request as URLRequest) { data, response, error in
             defer { self.dataTask = nil }
@@ -39,23 +39,16 @@ class ViewController: UIViewController {
                 print(self.errorMessage)
             } else if let data = data,
                 let response = response as? HTTPURLResponse,
-                1 == 1
-                {
+                response.statusCode == 200 // change to check response code
+            {
                 print(data)
                 print(response)
             }
-
+            
         }
         dataTask?.resume()
         
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
